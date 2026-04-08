fcf73f3dbda41e7321f3942105398a70a413db5b
adventurescumm <gab05mas@gmail.com>
2026-04-08 11:02:21 -0300

    Modificações feitas

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-x65bk1hkee81c774.us.auth0.com"
-      clientId="kFjkpeVhXxhTGY4zKMh4ytjHhOWmdYYc"
+      domain="dev-7ss74shg5ie3a0l5.us.auth0.com"
+      clientId="3sxDi31vcDlJUI4DFYxVVmqMSER6JUD3"
       authorizationParams={{
-        audience: "https://dev-x65bk1hkee81c774.us.auth0.com/api/v2/",
+        audience: "https://dev-7ss74shg5ie3a0l5.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
